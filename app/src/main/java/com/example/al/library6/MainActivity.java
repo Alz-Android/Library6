84f9fca6500aae216b96cfcef0453704d77d4956
Alz-Android <apiran06@gmail.com>
2016-08-21 23:03:38 -0700

    feature: adding GCM GCM client doesn't work and RegistrationIntentService has not been created.

--- app/src/main/java/com/example/al/library6/MainActivity.java
+++ app/src/main/java/com/example/al/library6/MainActivity.java
@@ -8,14 +8,35 @@ import android.util.Log;
 import android.view.View;
 
 import com.example.al.androidjoke.AndroidLibActivity;
-
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        checkPlayServices();
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkPlayServices();
+    }
+
+    private boolean checkPlayServices() {
+        GoogleApiAvailability googleAPI = GoogleApiAvailability.getInstance();
+        int result = googleAPI.isGooglePlayServicesAvailable(this);
+        if(result != ConnectionResult.SUCCESS) {
+            if(googleAPI.isUserResolvableError(result)) {
+                //TODO: constant 1 used temporarily for Error Dialog
+                googleAPI.getErrorDialog(this, result, 1).show();
+            }
+            return false;
+        }
+        return true;
     }
 
     public void launchLibraryActivity(View view){
@@ -23,6 +44,4 @@ public class MainActivity extends AppCompatActivity {
         Intent myIntent = new Intent(this, AndroidLibActivity.class);
         startActivity(myIntent);
     }
-
-
 }
